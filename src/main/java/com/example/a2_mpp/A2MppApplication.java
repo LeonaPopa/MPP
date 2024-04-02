e9309b2a7761a7b94c3e61e41f9984365ab60366
LeonaPopa <115133968+LeonaPopa@users.noreply.github.com>
2024-04-02 11:21:42 +0300

    Assignment1
    Front end master detail with crud operations

---
+++ src/main/java/com/example/a2_mpp/A2MppApplication.java
@@ -0,0 +1,13 @@
+package com.example.a2_mpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2MppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A2MppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/a2_mpp/A2MppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a2_mpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2MppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
